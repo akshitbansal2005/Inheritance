ebdc871373ea5aa8445aae2d860abd82d4af771d
Akshit Bansal <155195875+akshitbansal2005@users.noreply.github.com>
2024-10-16 12:06:37 +0530

    Create Single_Inheritence1.cpp

---
+++ Single_Inheritence1.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+class Uni {
+public:
+    string uni; 
+    Uni() : uni("Symbiosis: ") {}
+
+    void discipline() {
+        cout << "Engineering" << endl;
+    }
+};
+
+class Dep : public Uni {
+public:
+    string dept;
+    Dep() : dept("Electronics & Communication") {}
+};
+
+int main() {
+    Dep u1; 
+    u1.discipline(); 
+    cout << u1.uni + u1.dept << endl; 
+    return 0; 
+}
+//Output
+/*Engineering
+Symbiosis: Electronics & Communication
+*/
